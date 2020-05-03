6d805c7056907d0f934f50b596ec6b86511273b6
Silent404 <silent404lov@gmail.com>
2020-05-03 13:21:45 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+    name: "DZNEmptyDataSet",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v10)
+    ],
+    products: [
+        .library(name: "DZNEmptyDataSet", targets: ["DZNEmptyDataSet"]),
+    ],
+    targets: [
+        .target(
+            name: "DZNEmptyDataSet",
+            path: "Sources"
+        ),
+    ]
+)
